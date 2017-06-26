9be6909271bf8002689307fe622e5bb2e2d271e2
Amanda Kirchner <geeky.brewster@gmail.com>
2017-06-26 13:40:16 -0500

    Steps 4-6 completed.

--- client.js
+++ client.js
@@ -5,6 +5,13 @@ $(document).ready(function(){
     $('.container').append('<div> </div>');
     var $el = $(".container").children().last();
     $el.append('<p>You have clicked the button: ' + counter + ' times. </p>');
+    $el.append('<button id="swap">Swap</button>');
+    $el.append('<button id="delete">Delete</button>');
+  });
+
+  $('.container').on('click','#swap', function(){
+    $(this).parent().css({'background-color': 'yellow', 'color': 'black'});
+    // console.log($(this).parent());
   });
 
 
